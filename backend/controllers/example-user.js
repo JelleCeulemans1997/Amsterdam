d0a3b87e1f791a375d2ee24f22c9889a9f991abd
Julian <r0697475@student.thomasmore.be>
2019-12-09 11:03:16 +0100

    backend werkt

--- backend/controllers/example-user.js
+++ backend/controllers/example-user.js
@@ -36,7 +36,7 @@ router.post('/register', async (req, res) => {
         const user = new User(req.body)
         await user.save()
         // const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
+        res.status(201).send({ user })
     } catch (error) {
         res.status(400).send(error)
     }
--- backend/models/User.js
+++ backend/models/User.js
@@ -40,8 +40,6 @@ userSchema.methods.generateAuthToken = async function() {
     // Generate an auth token for the user
     const user = this
     const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
-    user.tokens = user.tokens.concat({token})
-    await user.save()
     return token
 }
 
