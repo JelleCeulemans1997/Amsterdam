da8525dc207647394bd89973d64387500db4f03f
vanhouttejan4 <vanhouttejan@telenet.be>
2019-12-09 10:39:05 +0100

    component signupmakers

---
+++ src/app/components/sign-up/sign-up-makers/sign-up-makers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-makers',
+  templateUrl: './sign-up-makers.component.html',
+  styleUrls: ['./sign-up-makers.component.scss']
+})
+export class SignUpMakersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/sign-up/sign-up.module.ts
+++ src/app/components/sign-up/sign-up.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { MaterialModule } from 'src/app/material.module';
 import { SharedModule } from 'src/app/shared.module';
+import { SignUpMakersComponent } from './sign-up-makers/sign-up-makers.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SignUpMakersComponent],
   imports: [
     CommonModule,
     MaterialModule,
