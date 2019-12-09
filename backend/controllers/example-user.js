dc072afa8805ab93f781ac3e54b00bb7ca9e1276
Julian <r0697475@student.thomasmore.be>
2019-12-09 10:38:00 +0100

    initialize

--- backend/controllers/example-user.js
+++ backend/controllers/example-user.js
@@ -16,6 +16,7 @@
 //           message: 'User created!',
 //           result: result
 //         });
+<<<<<<< Updated upstream
 //       })
 //       .catch(err => {
 //         res.status(500).json({
@@ -60,3 +61,68 @@
 //       });
 //     });
 // }
+=======
+//     } catch (error) {
+//         res.status(400).send(error)
+//     }
+// });
+
+router.post('/register', async (req, res) => {
+    // Create a new user
+    try {
+        const user = new User(req.body)
+        await user.save()
+        // const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// router.post('/sendmail', async (req, res) => {
+//     // Create a new user
+//     try {
+//         // const user = new User(req.body)
+//         const msg = {
+//             to: req.body.email,
+//             from: "support@pollinizer.com",
+//             templateId: 'd-8f1cd5f9702b4533903f9342b0d4cef4',
+//             subject: 'Join your Friend on PollPalls',
+//             dynamic_template_data: {
+//                 email: req.body.email,
+//                 subject: 'Join your Friend on PollPalls',
+//                 link: "https://www.ajaxshowtime.com",
+//             },
+//         };
+//         sgMail.send(msg);
+//         // await user.save()
+
+//         res.status(201).send("mail send");
+//     } catch (error) {
+//         res.status(400).send(error)
+//     }
+// })
+
+router.post('/user/login', async(req, res) => {
+    //Login a registered user
+    try {
+        const { email, password } = req.body
+        const user = await User.findByCredentials(email, password)
+        if (!user) {
+            return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+        }
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
+})
+
+router.get('/user/me', auth, async(req, res) => {
+    // View logged in user profile
+    res.send(req.user)
+})
+
+module.exports = router
+>>>>>>> Stashed changes
