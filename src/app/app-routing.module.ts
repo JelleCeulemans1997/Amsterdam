6289c280387faedc7a6afec6321ce9aea225a7fa
aswevers <amber.swevers@hotmail.com>
2019-12-13 16:11:13 +0100

    dev profile with reviews

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import { DeveloperDashboardComponent } from './components/developer/developer-da
 import { AuthGuard } from './components/auth/auth.guard';
 import { CompanyProfileComponent } from './components/company/company-profile/company-profile.component';
 import { DeveloperCredentialsComponent } from './components/developer/developer-credentials/developer-credentials.component';
+import { DeveloperProfileComponent } from './components/developer/developer-profile/developer-profile.component';
 
 
 
@@ -32,7 +33,8 @@ const routes: Routes = [
   { path: 'companyCredentials', component: CompanyCredentialsComponent, canActivate: [AuthGuard], data: {role: Role.Company}},
   { path: 'developerDashboard', component: DeveloperDashboardComponent, canActivate: [AuthGuard], data: {role: Role.Developer}},
   { path: 'companyProfile/:creatorId', component: CompanyProfileComponent, canActivate: [AuthGuard], data: {role: Role.Company}},
-  { path: 'developerCredentials', component: DeveloperCredentialsComponent, canActivate: [AuthGuard], data: {role: Role.Developer} }
+  { path: 'developerCredentials', component: DeveloperCredentialsComponent, canActivate: [AuthGuard], data: {role: Role.Developer} },
+  { path: 'developerProfile/:userId', component: DeveloperProfileComponent, canActivate: [AuthGuard], data: {role: Role.Developer} }
 ];
 
 @NgModule({
